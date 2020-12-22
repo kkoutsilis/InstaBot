6820f3984fb5e33642398536d60d7b4457e30e06
Konstantinos Koutsilis <50744802+koutsilis1999@users.noreply.github.com>
2020-12-22 19:27:16 +0200

    added inputs in command line so that every one can use is more easily

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from random import randint
-import mypass
-
+from getpass import getpass
 
 class InstaBot:
-    def __init__(self,username,password):
+    def __init__(self,username,password,post_url,comments):
         PATH = ("C:\Program Files (x86)\chromedriver.exe")
         self.driver = webdriver.Chrome(PATH)
-        self.driver.get("https://www.instagram.com/p/CIWEkK0rCIa/")
+        self.driver.get(post_url)
         time.sleep(2)
 
         #clicks the popup accept button
@@ -32,20 +31,20 @@ class InstaBot:
                 EC.presence_of_element_located((By.XPATH,"/html/body/div[1]/section/main/div/div/div/div/button"))
             )
             element.click()
-            # self.driver.switch_to_alert
-            # self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[3]/button[2]").click()
-            # self.driver.switch_to_default_content
 
             #COMMENTS for ever
+            print("Now the bot spams every 4 to 5 minutes the comment that you entered")
+    
             while True:
                 self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[1]/span[2]/button").click()
                 time.sleep(2)
-                self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[3]/div/form/textarea").send_keys("@jason_mp_ @kazantzi_ @apollongaitan")
+                self.driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[3]/div/form/textarea").send_keys(comments)
                 time.sleep(2)   
                 send = WebDriverWait(self.driver, 10).until(
                     EC.presence_of_element_located((By.XPATH,"/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[3]/div/form/button"))
                 )
                 send.click()
+                
                 time.sleep(randint(240,300))
         except:
             self.driver.quit()
@@ -67,22 +66,6 @@ class InstaBot:
         )
         profile.click()
         time.sleep(4)     
-    
-    #TODO send message to jason and not to the 3rd prifile in the message list 
-    def send_message(self):
-        self.driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[2]/a").click()
-        jason = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH,"/html/body/div[1]/section/div/div[2]/div/div/div[1]/div[3]/div/div/div/div/div[3]/a"))
-        )
-        jason.click()
-        time.sleep(2)    
-
-        self.driver.find_element_by_xpath("/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea").send_keys("Hello Jason , this is an automated message from instaBot created by kkouts!")
-        send = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH,"/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[3]/button"))
-        )
-        send.click()
-        time.sleep(4)
 
     def login(self,username,password):
         self.driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)
@@ -91,4 +74,10 @@ class InstaBot:
         
 
 
-InstaBot("konstantinos_koutsilis", mypass.password)
+
+
+username = input("Give your instagram username here: " )
+password = getpass("Give your instagram password here: ")
+post_url = input("Paste the url of the giveaway post here: ")
+comments = input("Give the comment that the bot will spam here" )
+InstaBot(username,password,post_url,comments)
